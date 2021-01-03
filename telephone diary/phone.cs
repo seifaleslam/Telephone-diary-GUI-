e42a3c2c657f2fb291e9f07fbe08242b5eaf7ee5
seifaleslam <seifaleslam2011@gmail.com>
2021-01-03 14:11:23 +0200

    initial commit

---
+++ telephone diary/phone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace telephone_diary
+{
+    public partial class phone : Form
+    {
+        public phone()
+        {
+            InitializeComponent();
+        }
+
+        private void phone_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            comboBox1.SelectedIndex = -1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
